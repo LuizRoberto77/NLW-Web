d1441788a5d99e921dead8eebc5e3b1fae39f85a
luizroberto77 <roberto-hero@hotmail.com>
2020-10-29 13:55:45 -0300

    primeiro commit do web

---
+++ src/utils/mapIcon.ts
@@ -0,0 +1,12 @@
+import Leaflet from 'leaflet';
+
+import mapMarkerimg from '../Images/Local.svg';
+
+const mapIcon = Leaflet.icon({
+    iconUrl: mapMarkerimg,
+    iconSize:[58,68],
+    iconAnchor:[29,68],
+    popupAnchor:[170,2] 
+});
+
+export default mapIcon;
